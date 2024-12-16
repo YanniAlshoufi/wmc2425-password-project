eb1093bfb68299fda315e1be124d5b522c20a6b1
YanniAlshoufi <yanni.school.work@gmail.com>
2024-12-16 10:17:08 +0100

    Add pepper env variable inject

--- src/main/java/at/htlleonding/users/UsersResource.java
+++ src/main/java/at/htlleonding/users/UsersResource.java
@@ -4,17 +4,25 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Slf4j
 @Path("/api/v1/users")
 public class UsersResource {
     private final UserPasswordResettingService resettingService;
     private final UserSigningService signingService;
+    private final String pepper;
 
-    public UsersResource(UserPasswordResettingService resettingService, UserSigningService signingService) {
+    public UsersResource(
+            UserPasswordResettingService resettingService,
+            UserSigningService signingService,
+            @ConfigProperty(name = "QUARKUS_PASSWORDS_HASH_PEPPER") String pepper
+    ) {
         this.resettingService = resettingService;
         this.signingService = signingService;
+        this.pepper = pepper;
     }
+
 
     @POST
     @Path("/sign-in")
